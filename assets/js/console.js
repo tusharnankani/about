ff53605964ae5b967467e7aa6860ffff67f2c2cd
tusharnankani <tusharnankani3@gmail.com>
2021-07-19 15:58:05 +0530

    :sparkles: feat: manage console

--- assets/js/console.js
+++ assets/js/console.js
@@ -1,7 +1,8 @@
-const asciiArt = () => {
+const manageConsole = () => {
+	console.clear();
 	console.log(
 		'%cThis place is where I was supposed to post job openings, but currently I need one :P',
-		'background: #212121; color: #f69510; padding: 10px; font-size: 16px '
+		'background: #f4f4f4; color: #001d3d; padding: 10px; margin: 20px 0px; font-size: 16px;  border-radius: 2px'
 	);
 	console.log(
 		`
@@ -15,11 +16,14 @@ const asciiArt = () => {
 	);
 
 	console.log(
-		"%c Let's talk! :D",
-		'background: #212121; color: #f69510; padding: 6px;'
+		"%c Let's talk! :D ",
+		'background: #000814; color: #f4f4f4; padding: 6px; border-radius: 5px; margin: 10px; font-size: 16px'
 	);
 	console.log = () => {};
 	console.error = () => {};
+	console.info = () => {};
+	console.warn = () => {};
+	console.clear = () => {};
 };
 
-asciiArt();
+manageConsole();
